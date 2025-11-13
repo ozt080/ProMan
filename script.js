7878b47b8ac06dad5217ffaa655649981f113048
unknown <berkanoztas11@gmail.com>
2025-11-13 10:58:47 +0100

    Modif 1

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function playFilm(src) {
-  const player = document.getElementById('player');
-  const video = document.getElementById('videoPlayer');
-  video.src = src;
-  player.style.display = 'block';
-  window.scrollTo(0, document.body.scrollHeight);
+    const video = document.getElementById('videoPlayer');
+    const container = document.getElementById('video-container');
+    video.src = src;
+    container.style.display = 'block';
+    window.scrollTo(0, document.body.scrollHeight);
 }
+
+
+document.getElementById('searchInput').addEventListener('input', function () {
+  const query = this.value.toLowerCase();
+  const films = document.querySelectorAll('.film');
+  films.forEach(film => {
+    const title = film.querySelector('.film-title').textContent.toLowerCase();
+    film.style.display = title.includes(query) ? 'block' : 'none';
+  });
+});
